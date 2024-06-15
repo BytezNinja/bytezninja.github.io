bd2d03a8ff68091d945b5058c46725c0f67540e3
Dhrubo Mondal <contact.dhrubomondal@gmail.com>
2024-06-15 08:58:29 +0530

    Changed icon

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,28 +1,27 @@
 import React from "react";
-
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 import { motion } from "framer-motion";
-import { textVariant, slideIn} from "../utils/motion";
+import { textVariant, slideIn } from "../utils/motion";
 import { styles } from "../styles";
 
 const Tech = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-center`}>
+      <motion.div variants={textVariant()} className="text-center">
+        <p className={styles.sectionSubText}>
           What I know 
         </p>
-        <h2 className={`${styles.sectionHeadText} text-center`}>
+        <h2 className={styles.sectionHeadText}>
           Technology
         </h2>
       </motion.div>
-      <motion.p
+      <motion.div
         variants={slideIn("right", "", 0.3, 1)}
-        className='flex flex-row flex-wrap justify-center mt-20 gap-10'
+        className='flex flex-wrap items-center justify-center mt-20 gap-10'
       >
         {technologies.map((technology) => (
-          <div className='w-28 h-28' key={technology.name}>
+          <div className='w-28 h-28 flex items-center justify-center' key={technology.name}>
             <img
               src={technology.icon}
               alt={technology.name}
@@ -30,7 +29,7 @@ const Tech = () => {
             />
           </div>
         ))}
-        </motion.p>
+      </motion.div>
     </>
   );
 };
